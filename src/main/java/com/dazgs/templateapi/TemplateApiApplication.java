d2ef8620f2f3bcc7e6d0805d1641a31bd79c248d
vanderZet-dev <85804034+vanderDB@users.noreply.github.com>
2021-09-08 23:10:43 +0300

    initial

---
+++ src/main/java/com/dazgs/templateapi/TemplateApiApplication.java
@@ -0,0 +1,13 @@
+package com.dazgs.templateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dazgs/templateapi/TemplateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dazgs.templateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
