bfde93f401c2bb829927e789410f237baadffc45
vanderZet-dev <85804034+vanderDB@users.noreply.github.com>
2021-09-11 23:50:14 +0300

    create simple repository, add user entity, add UserService and get all users controller method. Connect my sql db

--- src/main/java/com/dazgs/templateapi/web/controller/rest/UserController.java
+++ src/main/java/com/dazgs/templateapi/web/controller/rest/UserController.java
@@ -1,5 +1,9 @@
 package com.dazgs.templateapi.web.controller.rest;
 
+import com.dazgs.templateapi.core.domains.sql.User;
+import com.dazgs.templateapi.core.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/v1/users")
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/test")
     public ResponseEntity<String> getTestUserName() {
         return new ResponseEntity<>("TestName", HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Page<User>> findAll(Pageable pageable) {
+
+        return new ResponseEntity<>(userService.findAll(pageable), HttpStatus.OK);
     }
 }
