49ef8c6311232dfe68d87ac0451ba72ecbebea3c
vanderZet-dev <85804034+vanderDB@users.noreply.github.com>
2021-10-10 12:10:22 +0300

    add in memory auth

---
+++ src/main/java/com/dazgs/templateapi/configuration/security/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.dazgs.templateapi.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("myadmin").password("myadminpassword").roles("AADMIN").build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("myuser").password("myuserpassword").roles("USER").build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+}
