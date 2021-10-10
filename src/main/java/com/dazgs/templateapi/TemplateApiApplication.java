6549a53384b0effbe65a2ab6aa5970a6b7267854
vanderZet-dev <85804034+vanderDB@users.noreply.github.com>
2021-10-10 20:27:57 +0300

    move to config securedEnabled setting

--- src/main/java/com/dazgs/templateapi/TemplateApiApplication.java
+++ src/main/java/com/dazgs/templateapi/TemplateApiApplication.java
@@ -2,14 +2,12 @@ package com.dazgs.templateapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
-@EnableGlobalMethodSecurity(securedEnabled = true)
 public class TemplateApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TemplateApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateApiApplication.class, args);
+    }
 
 }
--- src/main/java/com/dazgs/templateapi/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/dazgs/templateapi/configuration/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.dazgs.templateapi.configuration.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
